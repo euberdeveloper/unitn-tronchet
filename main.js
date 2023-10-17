0f38d3d1da80659ca04aff0f69c925fde297a661
euberdeveloper <euberdeveloper@gmail.com>
2023-10-17 22:21:21 +0200

    refactor: comment out firebase
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const DIST_FOLDER = path.join(__dirname, 'frontend');
 
 const redirect = require('./utilities/redirect');
 
+/*
 const firebaseAdmin = require('firebase-admin');
 const serviceAccount = require('./utilities/firebase-credentials')();
 firebaseAdmin.initializeApp({
@@ -22,6 +23,7 @@ firebaseAdmin.initializeApp({
 });
 const firebaseMessaging = firebaseAdmin.messaging();
 const PUBLIC_TOPIC = "public";
+*/
 
 app.use(compression());
 if (process.env.NODE_ENV === 'production') {
@@ -39,12 +41,14 @@ app.use(morgan('dev'));
 app.use(history());
 app.use(express.static(DIST_FOLDER));
 
+/*
 app.post('/api/register-token', (req, res) => {
     const { token } = req.body;
     firebaseMessaging.subscribeToTopic(token, PUBLIC_TOPIC)
         .then(_ => res.status(200).send({ success: 'Token added succesfully to the topic'} ))
         .catch(error => res.status(500).send({ message: 'Error in adding token to the topic', error }));
 });
+*/
 
 app.listen(PORT, () => {
     console.log('PORT ', PORT);
